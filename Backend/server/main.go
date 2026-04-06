915d325bc74f03c12ca0062c29e526a264c0e7bf
Rintaro <sirime2351develop@gmail.com>
2026-04-06 20:32:08 +0900

    簡単にAPIの仮実装

--- Backend/server/main.go
+++ Backend/server/main.go
@@ -1,13 +1,44 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
 
-func main() {
-	r := gin.Default()
-	r.GET("/test", func(c *gin.Context) {
-		c.JSON(200, gin.H{
+	"github.com/gin-gonic/gin"
+)
+
+
+func testHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
 			"message": "ok",
 		})
-	})
-	r.Run()
+		// if err := c.ShouldBindJSON($req) != nil
+		// c.JSON(http.statusBadRequest, gin.H{"error" : "invalid request"})
+}
+
+func loginHandler(c *gin.Context) {
+	var req Auth
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error" : "invalid request"})
+		return
+	}
+	fmt.Println("Username", req.Username)
+	fmt.Println("Password", req.Password)
+	
+	c.JSON(http.StatusOK, gin.H{
+	"message":"login success!",
+	"Username":req.Username,
+})
+}
+
+type Auth struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/test", testHandler)
+	router.POST("/login", loginHandler)
+	router.Run()
 }
